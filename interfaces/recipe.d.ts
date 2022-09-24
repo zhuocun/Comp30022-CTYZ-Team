05b2a71ef31b00fe6c58ca4a7028aa274b2ba464
zhuocun <zhuocun.tang@outlook.com>
2022-09-24 19:27:54 +1000

    feat: api for recipe (CUD)

---
+++ interfaces/recipe.d.ts
@@ -0,0 +1,5 @@
+interface Recipe {
+    pic: string;
+    title: string;
+
+}
---
+++ redux/reducers/recipeSlice.ts
@@ -0,0 +1,131 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+
+interface RecipeState {
+    loading: boolean;
+    error: string | null;
+    recipe: Recipe | null;
+}
+
+const initialState: RecipeState = {
+    loading: false,
+    error: null,
+    recipe: null
+};
+
+export const createRecipe = createAsyncThunk(
+    "recipe/createRecipe",
+    async (parameters: {
+        jwtToken: string | null, recipe: Recipe | null
+    }) => {
+        const axiosResponse = await axios.post(
+            ``,
+            {
+                recipe: parameters.recipe
+            },
+            {
+                headers: {
+                    tokens: `${parameters.jwtToken}`
+                }
+            }
+        );
+        return axiosResponse.data.token;
+    }
+);
+
+export const updateRecipe = createAsyncThunk(
+    "recipe/updateRecipe",
+    async (parameters: {
+        jwtToken: string | null, recipeId: string | null, recipe: Recipe | null
+    }) => {
+        const axiosResponse = await axios.put(
+            ``,
+            {
+                recipeId: parameters.recipeId,
+                recipe: parameters.recipe
+            },
+            {
+                headers: {
+                    tokens: `${parameters.jwtToken}`
+                }
+            }
+        );
+        return axiosResponse.data.token;
+    }
+);
+
+export const deleteRecipe = createAsyncThunk(
+    "recipe/deleteRecipe",
+    async (parameters: {
+        jwtToken: string | null, recipeId: string | null
+    }) => {
+        const axiosResponse = await axios.delete(
+            ``,
+            {
+                headers: {
+                    tokens: `${parameters.jwtToken}`
+                }
+            }
+        );
+        return axiosResponse.data.token;
+    }
+);
+
+export const recipeSlice = createSlice({
+    name: "authentication",
+    initialState,
+    reducers: {
+        logout: (state) => {
+            state.loading = false;
+            state.error = null;
+            state.recipe = null;
+        }
+    },
+    extraReducers: {
+        [createRecipe.pending.type]: (state) => {
+            state.loading = true;
+        },
+        [createRecipe.fulfilled.type]: (state, action) => {
+            state.loading = false;
+            state.error = null;
+            state.recipe = action.payload;
+        },
+        [createRecipe.rejected.type]: (
+            state,
+            action: PayloadAction<Recipe | null>
+        ) => {
+            state.loading = false;
+            state.recipe = action.payload;
+        },
+        [updateRecipe.pending.type]: (state) => {
+            state.loading = true;
+        },
+        [updateRecipe.fulfilled.type]: (state, action) => {
+            state.loading = false;
+            state.error = null;
+            state.recipe = action.payload;
+        },
+        [updateRecipe.rejected.type]: (
+            state,
+            action: PayloadAction<Recipe | null>
+        ) => {
+            state.loading = false;
+            state.recipe = action.payload;
+        },
+        [deleteRecipe.pending.type]: (state) => {
+            state.loading = true;
+        },
+        [deleteRecipe.fulfilled.type]: (state, action) => {
+            state.loading = false;
+            state.error = null;
+            state.recipe = action.payload;
+        },
+        [deleteRecipe.rejected.type]: (
+            state
+        ) => {
+            state.loading = false;
+            state.recipe = null;
+        }
+
+    }
+});
