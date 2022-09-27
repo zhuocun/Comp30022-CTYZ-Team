305cf2ae700d56e69ae11493a9969e91968350fc
qiyuezhang <qiyuezhang@student.unimelb.edu.au>
2022-09-28 01:30:03 +1000

    add a new homepage

---
+++ pages/homepage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NextPage } from "next";
+import Snack from "/public/snack.svg";
+import Dessert from "/public/dessert.svg";
+import Breakfast from "/public/breakfast.svg";
+import Lunch from "/public/lunch.svg";
+import Dinner from "/public/dinner.svg";
+import Vegetarian from "/public/vegetarian.svg";
+
+const HomePage: NextPage = () => {
+    return (
+            <div>
+                <Snack/>
+                <Dessert/>
+                <Breakfast/>
+                <Lunch/>
+                <Dinner/>
+                <Vegetarian/>
+            </div>
+
+    );
+};
+
+export default HomePage;
