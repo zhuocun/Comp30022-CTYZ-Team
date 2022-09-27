6409897482d4cb1cccbb9bad134f13f25a3cf0e6
shiweny1 <shiwen.ye@student.unimelb.edu.au>
2022-09-28 02:19:18 +1000

    Added categories on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,9 @@ import { SearchBar } from "../components/searchBar";
 import { Layout } from "antd";
 import styles from "../styles/index.module.css";
 import Link from "next/link";
-import {
-    HistoryOutlined,
-    UserOutlined,
-} from "@ant-design/icons";
+import { HistoryOutlined, UserOutlined } from "@ant-design/icons";
 import FooterNavBar from "../components/footerNavBar";
+import Category from "../components/category";
 
 const { Header, Content, Footer } = Layout;
 
@@ -33,10 +31,12 @@ const Home: NextPage = () => {
                 </div>
             </Header>
             <Content className={styles["content"]}>
-                <div>
+                <div className={styles["searchBar"]}>
                     <SearchBar />
                 </div>
-                content
+                <div className={styles["category"]}>
+                    <Category />
+                </div>
             </Content>
             <Footer className={styles["footer"]}>
                 <FooterNavBar />
