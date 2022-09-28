62c52909be5e1e19e4ce1135e299376809e183a3
shiweny1 <shiwen.ye@student.unimelb.edu.au>
2022-09-28 17:30:38 +1000

    optimize the page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-    Route,
-    Routes,
-    MemoryRouter as Router
-} from "react-router-dom";
+import { Route, Routes, MemoryRouter as Router } from "react-router-dom";
 
 import FooterNavBar from "../components/footerNavBar";
 import styles from "../styles/index.module.css";
@@ -14,15 +10,13 @@ import Home from "./homepage";
 export default () => {
     return (
         <Router initialEntries={["/homepage"]}>
-            <div className={styles.app}>
-                <div className={styles.body}>
-                    <Routes>
+            <div>
+                <Routes>
                     <Route path="/homepage" element={<Home />} />
-                        <Route path="/favoriteList" element={<FavoriteList />} />
-                        <Route path="//favoriteList" element={<FavoriteList/>} />
-                        <Route path="/historyList" element={<HistoryList/>} />
-                    </Routes>
-                </div>
+                    <Route path="/favoriteList" element={<FavoriteList />} />
+                    <Route path="//favoriteList" element={<FavoriteList />} />
+                    <Route path="/historyList" element={<HistoryList />} />
+                </Routes>
                 <div className={styles.bottom}>
                     <FooterNavBar />
                 </div>
