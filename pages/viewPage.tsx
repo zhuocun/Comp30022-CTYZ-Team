34ae645b392cb38cfe31815b8a80fefc7c8f0eb4
shiweny1 <shiwen.ye@student.unimelb.edu.au>
2022-09-30 22:38:57 +1000

    view recipe page structure

---
+++ pages/viewPage.tsx
@@ -0,0 +1,63 @@
+import { NextPage } from "next";
+import React from "react";
+import { Layout, Row } from "antd";
+import styles from "../styles/viewPage.module.css";
+import { Image, Space } from "antd-mobile";
+import { LeftOutline, SendOutline } from "antd-mobile-icons";
+import Link from "next/link";
+import ECookLogo from "/public/logo.svg";
+import { CarryOutOutlined, HeartOutlined } from "@ant-design/icons";
+
+const { Header, Content, Footer } = Layout;
+
+const demoSrc =
+    "https://cookingwithayeh.com/wp-content/uploads/2021/11/Spicy-Tuna-Crispy-Rice.jpg";
+
+const ViewPage: NextPage = () => {
+    // const jwtToken = useReduxSelector((s) => s.authentication.jwtToken);
+    // const dispatch = useReduxDispatch();
+
+    // useEffect(() => {
+    //     if (jwtToken) {
+    //         dispatch(getRecipeList({ jwtToken, keywords: undefined }));
+    //     }
+    // }, [jwtToken]);
+
+    return (
+        <Layout>
+            <Header className={styles["header"]}>
+                <div className={styles["navigation"]}>
+                    <Link href="/recipes">
+                        <span className={styles["return"]}>
+                            <LeftOutline />
+                        </span>
+                    </Link>
+                    <ECookLogo />
+                    <Link href="/">
+                        <span className={styles["share"]}>
+                            <SendOutline />
+                        </span>
+                    </Link>
+                </div>
+                <div style={{ userSelect: "none" }} className={styles["img"]}>
+                    <Image src={demoSrc} className={styles["image"]}/>
+                </div>
+                <div className={styles["functions"]}>
+                
+                        <CarryOutOutlined className={styles["complete"]} twoToneColor="yellow"/>
+               
+                    <div className={styles["title"]}>
+                    <h1>Spicy Tuna Cripsy Rice</h1>
+                    </div>
+              
+                        <HeartOutlined className={styles["favorite"]}/>
+                  
+                </div>
+            </Header>
+            <Content className={styles.content}>content</Content>
+            <Footer className={styles.footer}>footer</Footer>
+        </Layout>
+    );
+};
+
+export default ViewPage;
